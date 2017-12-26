8c2fa321030b8cce4260c4bc9f40fee62985c8b5
kof1290 <kof1290ly@163.com>
2017-12-26 16:19:36 +0800

    datasave

---
+++ UsbVideo.py
@@ -0,0 +1,9 @@
+#encoding=utf-8
+#example https://coolshell.cn/articles/1928.html
+
+from VideoCapture import Device
+import sys,pygame,time
+from pygame.locals import *
+from PIL import ImageEnhance,ImageDraw
+
+res = (640,480)
