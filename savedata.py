c652dec948f249117be0302e6207c65cca9f7eae
kof1290 <kof1290ly@163.com>
2017-12-26 14:09:52 +0800

    datasave

--- savedata.py
+++ savedata.py
@@ -1,5 +1,8 @@
 #encoding=utf-8
 #example for datasave
+#最后需要写一个datasave的services 在开机之后就运行
+#注意判断各种状态 以及不能存储的数据
+#表的结构 需要设定好 主键为设备id 应从usb摄像头id得到对应关系
 import pymssql
 
 conn=pymssql.connect(host='.',user='sa',password='123456',database='ReportServerTempDB')
