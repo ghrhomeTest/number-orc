7a8a0b8dd9041796fc25681ff657312397983f89
kof1290 <kof1290ly@163.com>
2018-01-04 13:54:11 +0800

    一些小改动

--- IPcam.py
+++ IPcam.py
@@ -3,10 +3,13 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("rtmp://192.168.24.13/live/live")
+url = 'rtmp://192.168.24.13/live/live'
+cap = cv2.VideoCapture(url)
 is_open = cap.isOpened()
 fps = cap.get(cv2.CAP_PROP_FPS)
 size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+# c = 1
+# timeF = 1000
 
 # print(is_open)
 # print(fps)
@@ -16,6 +19,12 @@ size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_H
 while is_open:
     # print('已探测到摄像头')
     ret, frame = cap.read()
+    # 读取视频帧 这里先展示 应用imread
+    # if(c%timeF == 0):
+    #     print('youle ')
+    #     cv2.imshow('jpg', frame)
+    # c=c+1
+
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     # out.write(frame)
     # cv2.imshow('frame',frame)
